96d1f3e30e1f4db4c21c68073c3724226612c0e2
POMAH-KOPOJlb <POMAH-KOPOJlb>
2022-11-20 03:53:39 +0300

    API integration

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import AppRouter from './router/AppRouter';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.scss';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+       <AppRouter/>
+  
+);
