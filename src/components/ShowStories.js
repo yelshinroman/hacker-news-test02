50c1184598f54d49c943f4f5189b007436368215
POMAH-KOPOJlb <POMAH-KOPOJlb>
2022-11-21 21:17:35 +0300

    Basic version is done

--- src/components/ShowStories.js
+++ src/components/ShowStories.js
@@ -1,22 +1,19 @@
 import React from 'react';
 import Story from './Story';
 import useDataFetcher from '../hooks/dataFetcher';
+import Loader from './Loader';
 
-const ShowStories = (props) => {
-  const { type } = props.match.params;
-  const { isLoading, stories } = useDataFetcher(type);
+const ShowStories = ({ type }) => {
+  const { isLoading, stories } = useDataFetcher(type ? type : 'top');
 
   return (
     <React.Fragment>
-      {isLoading ? (
-        <p className="loading">Loading...</p>
-      ) : (
-        <React.Fragment>
-          {stories.map(({ data: story }) => (
-            <Story key={story.id} story={story} />
-          ))}
-        </React.Fragment>
-      )}
+      <Loader show={isLoading}>Loading...</Loader>
+      <React.Fragment>
+        {stories.map(({ data: story }) => (
+          <Story key={story.id} story={story} />
+        ))}
+      </React.Fragment>
     </React.Fragment>
   );
 };
